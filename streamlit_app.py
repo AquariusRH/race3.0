ab628869041e551b7cf12eb2f4b7318419def43a
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-11-25 22:17:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1074,6 +1074,8 @@ def print_bubble():
             font=dict(family="Microsoft JhengHei", size=14),
             xaxis=dict(tickformat=",", zeroline=True),
             yaxis=dict(tickformat=",", zeroline=True),
+            dragmode=False,                    
+            static_plot=True
         )
         st.plotly_chart(fig, use_container_width=True, key=f"{total_volume_raw.index.astype(str)} {method}")  
         
