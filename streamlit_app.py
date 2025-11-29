b4fa5afdff6fb5341bcb2eebeca881da322a15c5
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-11-30 00:22:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1015,8 +1015,8 @@ def print_bubble():
         total_volume = total_volume_raw.copy()          
         df = pd.DataFrame({
             'horse': total_volume.columns.astype(str),
-            'ΔI': delta_I.round(0).astype(float),
-            'ΔQ': delta_Q.round(0).astype(float),
+            'ΔI': delta_I.round(0).astype(float)*6,
+            'ΔQ': delta_Q.round(0).astype(float)*6,
             '總投注量': total_volume.iloc[0].round(0).astype(int)
             })     
         # 4. 退賽馬直接不顯示氣泡（size 設為 0）＋淡化文字
