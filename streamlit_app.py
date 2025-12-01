49bf60fb8aed2d9dcd5cb20dbb7f4daf70b20fad
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-12-01 11:11:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1058,13 +1058,34 @@ def print_bubble():
             customdata=df['總投注量'][df['visible']].values
         ))
 
-
+        HK_TZ = timezone(timedelta(hours=8))
+        now_naive = datetime.now()
+        now = now_naive + datere.relativedelta(hours=8)
+        now = now.replace(tzinfo=HK_TZ)
+        post_time_raw = st.session_state.post_time_dict.get(race_no)
+        
+        if post_time_raw is None:
+            time_str = "未載入"
+        else:
+            # 確保 post_time 也有時區
+            if post_time_raw.tzinfo is None:
+                post_time = post_time_raw.replace(tzinfo=HK_TZ)
+            else:
+                post_time = post_time_raw  # 已有時區
+        
+            seconds_left = (post_time - now).total_seconds()
+            
+            if seconds_left <= 0:
+                time_str = "已開跑"
+            else:
+                minutes = int(seconds_left // 60)
+                time_str = f"離開跑 {minutes} 分"
         # 四象限線
         fig.add_hline(y=0, line_color="lightgrey", line_width=1.5)
         fig.add_vline(x=0, line_color="lightgrey", line_width=1.5)
 
         fig.update_layout(
-            title=f"{method}氣泡圖",
+            title=f"{method}氣泡圖 |  {time_str}",
             xaxis_title=f"{method_name[0]}（K）",
             yaxis_title=f"{method_name[1]}（K）",
             template="plotly_white",
